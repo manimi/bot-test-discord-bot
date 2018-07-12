81874de2ed0e7b3143d847fed214e2ef106dd286
Manimi The Fire H/R <manimifire@gmail.com>
2018-07-12 10:41:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 import discord
 from discord.ext import commands
+import Pymoe
+import os
 import random
 
-bot = discord.Client()
+#GETTING API KEYS FROM HEROKU
+api = os.environ["RIOT_KEY"]
+wu_key = os.environ['WU_API']
+owm = os.environ['open_weather']
+img_api = os.environ['img_api']
+apiai_token = os.environ['api_ai']
+bot_token = os.environ['BOT_TOKEN']
+An = Pymoe.Anilist()
+
 bot = commands.Bot(command_prefix='dd!')
 
 @bot.event
@@ -11,5 +21,17 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    
+@bot.command(pass_context=True)
+async def logout(ctx):
+    """RESTARTS THE BOT IN HEROKU SERVER, BUT ENDS IN TERMINAL"""
+    creator_id = 224185471826132992
+    sender_id = ctx.message.author.id
+    send_id = int(sender_id)
+    if send_id == creator_id:
+        await bot.say("Logging out bot now!")
+        await bot.logout()
+    else:
+        await bot.say("Can not restart bot because you are not the creator")
 
 bot.run(bot_token)
