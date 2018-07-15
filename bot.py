b1984863eafca2ad7a9819d3657db73e4d226d4b
Manimi The Fire H/R <manimifire@gmail.com>
2018-07-15 14:27:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,6 +45,6 @@ async def update(ctx):
     g = github.Github()
     repo = g.get_repo('manimi/bot-test-discord-bot')
     file = repo.get_file_contents("/update.json")
-    repo.update_file("/bot-test-discord-bot/update.json", "Commit Comments", "decoded_content", file.sha)
+    repo.update_file("/update.json", "Commit Comments", content, sha)
 
 bot.run(bot_token)
